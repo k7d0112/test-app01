45a3999118e6f215c0fbb4dc734083b8e0b59253
John Doe <ka7de0112@gmail.com>
2024-07-08 12:03:24 +0900

    課題4修正1

--- src/Blog.js
+++ src/Blog.js
@@ -5,45 +5,45 @@ import { useState, useEffect} from 'react';
 import './Blog.css';
 
 function Blog ({posts}) {
-    const navigate = useNavigate();
-    const handleNavigation = (id) => {
-        navigate(`/article/${id}`, {state: {loading: true}});
-    }
+  const navigate = useNavigate();
+  const handleNavigation = (id) => {
+      navigate(`/article/${id}`, {state: {loading: true}});
+  }
 
-    const formatDateHyphen = (dateString) => {
-        const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        return `${year}-${month}-${day}`;
-    }
-    const formatDateSlash = (dateString) => {
-        const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        return `${year}/${month}/${day}`;
-    }
+  const formatDateHyphen = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}-${month}-${day}`;
+  }
+  const formatDateSlash = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}/${month}/${day}`;
+  }
+
     return (
       <>
         {posts.map((post)=>{
-            return (
+          return (
             <li className='blog__item' key={post.id}>
-                <Link to={`/articles/${post.id}`} className='blog__link' onClick={() => handleNavigation(post.id)}>
-                    <div className='blog__related'>
-                        <time className='blog__time' dateTime={formatDateHyphen(post.createdAt)}>{formatDateSlash(post.createdAt)}</time>
-                        <ul className='blog__tag-list'>
-                        {post.categories.map((category,index)=><li className='blog__tag' key={index}>{category}</li>)}
-                        </ul>
-                    </div>
-                    <h1 className='blog__title'>{post.title}</h1>
-                    <p className='blog__text'>
-                        {post.content}
-                    </p>
-                </Link>
+              <Link to={`/articles/${post.id}`} className='blog__link' onClick={() => handleNavigation(post.id)}>
+                <div className='blog__related'>
+                  <time className='blog__time' dateTime={formatDateHyphen(post.createdAt)}>{formatDateSlash(post.createdAt)}</time>
+                  <ul className='blog__tag-list'>
+                  {post.categories.map((category,index)=><li className='blog__tag' key={index}>{category}</li>)}
+                  </ul>
+                </div>
+                <h1 className='blog__title'>{post.title}</h1>
+                <p className='blog__text'>
+                  {post.content}
+                </p>
+              </Link>
             </li>
-
-            );
+          );
         })}
       </>
     );
